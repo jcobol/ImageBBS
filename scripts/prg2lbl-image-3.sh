817b880cec21fbb8b30ee906c9d03c06e859d153
jcobol <springair@protonmail.com>
2025-11-01 14:54:44 -0400

    Add rotating backups to prg2lbl script (#294)

--- scripts/prg2lbl-image-3.sh
+++ scripts/prg2lbl-image-3.sh
@@ -9,8 +9,8 @@ E_NOTFOUND=86
 E_NOTPRG=87
 
 if [ $# -eq 0 ]
-	then # no command params
-		echo "Usage: `basename $0` <.prg_file> [<.lbl_file>]"
+        then # no command params
+                printf 'Usage: %s <.prg_file> [<.lbl_file>]\n' "$(basename "$0")"
 cat <<endofhelp
 
 De-tokenize a C64 binary .prg file into C64List 4.0 .lbl file.
@@ -27,8 +27,9 @@ if [ $# -eq 1 ] # 1 param: input_prg_file. output_lbl_file will be input_prg_fil
 then
 	input_prg_file=$1
 	output_lbl_file=${input_prg_file%.*}.lbl # cut .prg extension, replace with .lbl
-	echo "Converting $input_prg_file to $output_lbl_file"
-	wine c64list4_00.exe $input_prg_file -verbose -lbl:$output_lbl_file -ovr -keycase -varcase -crunch:off -alpha:alt -pref:image-3_0-assigns.lbl
+	printf 'Converting %s to %s\n' "$input_prg_file" "$output_lbl_file"
+	wine_args=("c64list4_00.exe" "$input_prg_file" -verbose "-lbl:$output_lbl_file" -ovr -keycase -varcase -crunch:off -alpha:alt -pref:image-3_0-assigns.lbl)
+	wine "${wine_args[@]}"
 	exit $?
 fi
 
@@ -38,37 +39,59 @@ then # 2 params: input_prg_file and output_lbl_file
 	output_lbl_file="$2"
 	if [ ! -f "$input_prg_file" ]
 	then
-		echo "File $input_prg_file not found!" >&2
+		printf 'File %s not found!\n' "$input_prg_file" >&2
 		exit $E_NOTFOUND
 	fi
 
-	if [ ${input_prg_file##*.} != "prg" ]
+	if [ "${input_prg_file##*.}" != "prg" ]
 	# use braces in variable substitution
 	then
-		echo "File $input_prg_file is not a .prg file!" >&2
-		exit E_NOTPRG
+		printf 'File %s is not a .prg file!\n' "$input_prg_file" >&2
+		exit $E_NOTPRG
 	fi
 
-	if [ ${output_lbl_file##*.} != "lbl" ]
+	if [ "${output_lbl_file##*.}" != "lbl" ]
 	# use braces in variable substitution
 	then
-		echo "File $output_lbl_file is not a .lbl file!" >&2
-		exit E_NOTPRG
+		printf 'File %s is not a .lbl file!\n' "$output_lbl_file" >&2
+		exit $E_NOTPRG
 	fi
 
-	# does $output_lbl_file already exist?
-	# if so, back it up with .bak extension
+	# Rotate numbered .bak backups (default depth is 5 or PRG2LBL_BACKUP_DEPTH).
 	if [ -f "$output_lbl_file" ]
 	then
-		echo "Backing up $output_lbl_file as $output_lbl_file.bak" >&1
-		# TODO: rotate backups? :P
-		cp "$output_lbl_file" "$output_lbl_file.bak"
+		backup_depth=${PRG2LBL_BACKUP_DEPTH:-5}
+		if [[ "$backup_depth" =~ ^[0-9]+$ ]] && (( backup_depth > 0 ))
+		then
+			if [ -f "${output_lbl_file}.bak.${backup_depth}" ]
+			then
+				rm -f -- "${output_lbl_file}.bak.${backup_depth}"
+			fi
+			if [ -f "${output_lbl_file}.bak" ]
+			then
+				mv -f -- "${output_lbl_file}.bak" "${output_lbl_file}.bak.1"
+			fi
+			for ((i=backup_depth; i>=2; i--))
+			do
+				prev=$((i-1))
+				if [ -f "${output_lbl_file}.bak.${prev}" ]
+				then
+					mv -f -- "${output_lbl_file}.bak.${prev}" "${output_lbl_file}.bak.${i}"
+				fi
+			done
+			cp -- "$output_lbl_file" "${output_lbl_file}.bak.1"
+			printf 'Backed up %s to %s.bak.1 (depth %s)\n' "$output_lbl_file" "$output_lbl_file" "$backup_depth"
+		else
+			printf 'Skipping backup rotation for %s because depth "%s" is not a positive integer.\n' "$output_lbl_file" "$backup_depth"
+		fi
 	fi
 
-	echo "Converting $input_prg_file to $output_lbl_file"
-	wine c64list4_00.exe $input_prg_file -verbose -lbl:$output_lbl_file -keycase -varcase -crunch:off -alpha:alt -pref:image-3_0-assigns.lbl
+	printf 'Converting %s to %s\n' "$input_prg_file" "$output_lbl_file"
+	wine_args=("c64list4_00.exe" "$input_prg_file" -verbose "-lbl:$output_lbl_file" -keycase -varcase -crunch:off -alpha:alt -pref:image-3_0-assigns.lbl)
+	wine "${wine_args[@]}"
+	wine_status=$?
 
 #    -d64:$d64file::`echo {$2}|tr `;
 fi
 
-exit $?
+exit ${wine_status:-$?}
